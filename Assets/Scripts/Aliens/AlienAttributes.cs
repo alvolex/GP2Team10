bce287a2735925776a7723eb34512f3e45b09864
f0jzd <tomassavela@gmail.com>
2022-01-25 17:06:32 +0100

    big push, very script big upgrade w0w

--- Assets/Scripts/Aliens/AlienAttributes.cs
+++ Assets/Scripts/Aliens/AlienAttributes.cs
@@ -20,6 +20,10 @@ public class AlienAttributes : MonoBehaviour
     [SerializeField] private IntReference aliensFedReference;
     [SerializeField] private ScriptableEventIntReference onAlienFed;
     
+    [Header("Allergens fed: ")]
+    [SerializeField] private IntReference allergensFedReference;
+    [SerializeField] private ScriptableEventIntReference onAllergenFed;
+    
     
 
     public Ingredients.Allergy[] allergy;
@@ -39,7 +43,9 @@ public class AlienAttributes : MonoBehaviour
             {
                 if (allergyInFood == alienAllergy)
                 {
+                    //TODO separate the allergy killing from the allergy just making them sick
                     Debug.Log("Allergy spotted, killed customer");
+                    CustomerIsAllergic();
                     Destroy(gameObject);
                     customerHasDied?.Invoke(gameObject.GetComponent<Customer>());
                     return;
@@ -50,6 +56,13 @@ public class AlienAttributes : MonoBehaviour
             
         }
     }
+
+    private void CustomerIsAllergic()
+    {
+        allergensFedReference.ApplyChange(+1);
+        onAllergenFed.Raise(allergensFedReference.GetValue());
+    }
+
     void FoodIsEdible()
     {
         aliensFedReference.ApplyChange(+1);
@@ -62,6 +75,15 @@ public class AlienAttributes : MonoBehaviour
         tipsReference.ApplyChange(+maxTip);
         onTipsChanged.Raise(aliensFedReference.GetValue());
         
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            aliensFedReference.ApplyChange(+1);
+            onAlienFed.Raise(aliensFedReference.GetValue());
+        }
     }
 }
 
--- Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
+++ Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
@@ -8,76 +8,183 @@ public class UpgradeSystem : MonoBehaviour
     [Header("References")] 
     [SerializeField] private GameObject playerReference;
     [SerializeField] private IntReference aliensReference;
+    [SerializeField] private IntReference allergensReference;
+    [SerializeField] private IntReference tipsReference;
     [SerializeField] private Button upgradeButton;
 
     [Header("Movement Speed")] 
     [SerializeField] private int upgrade1GoalMS;
     [SerializeField] private int upgrade2GoalMS;
     [SerializeField] private int upgrade3GoalMS;
+    
+    [Header("Allergens Served limits")] 
+    [SerializeField] private int allergensServedLimit1;
+    [SerializeField] private int allergensServedLimit2;
+    [SerializeField] private int allergensServedLimit3;
 
     [Header("Customer Movement Speed")] 
     [SerializeField] private int upgrade1GoalCMS;
     [SerializeField] private int upgrade2GoalCMS;
     [SerializeField] private int upgrade3GoalCMS;
+    
+    [Header("Upgrade Costs")]
+    [SerializeField] private int[] movementSpeedUpgradeCost;
+    [SerializeField] private int[] alienMovementSpeedUpgradeCost;
+    [SerializeField] private int[] cookingStationUpgradeSlot;
+    [SerializeField] private int[] storageSlotUpgradeCost;
+    [SerializeField] private int[] extraSeatingUpgradeCost;
+    
+    [Header("Cooking Stations")]
+    [SerializeField] private GameObject[] cookingStations;
+    
+    [Header("Tables")]
+    [SerializeField] private GameObject[] tables;
 
-    private int upgradesAvailable;
+    private int cookingStation;
+    private int table;
+    
+    private int movementSpeedUpgradesAvailable;
+    private int alienMovementSpeedUpgradesAvailable;
+    private int extraCookingStationUpgradesAvailable;
+    private int extraStorageSlot;
+    private int extraSeatingTable;
 
+    int currentMSUpgrade = 0;
+    int currentCMSUpgrade = 0;
+    int currentAllergenUpgrade = 0;
+    int currentCookingStationUpgrade = 0;
+    int currentStorageUpgrade = 0;
+    int currentSeatingUpgrade = 0;
+    
+    
 
     private void Start()
     {
         upgradeButton.interactable = false;
     }
-
     public void CheckAliensFed()
     {
         if (aliensReference.GetValue() == upgrade1GoalMS)
-        {
-            Debug.Log($"You have fed {upgrade1GoalMS} aliens now, noice");
-            upgradesAvailable++;
-        }
-
+            movementSpeedUpgradesAvailable++;
         if (aliensReference.GetValue() == upgrade2GoalMS)
-        {
-            Debug.Log($"You have fed {upgrade2GoalMS} aliens now, noice");
-            //Change values to whatever benchmark I guess?
-            upgradesAvailable++;
-        }
-
+            movementSpeedUpgradesAvailable++;
         if (aliensReference.GetValue() == upgrade3GoalMS)
-        {
-            Debug.Log($"You have fed {upgrade3GoalMS} aliens now, noice");
-            //Change values to whatever benchmark I guess?
-            upgradesAvailable++;
-        }
-
+            movementSpeedUpgradesAvailable++;
         CheckButton();
     }
-
-    public void CustomerThing()
+    public void CustomersWithAllergiesServed()
     {
+        if (allergensReference.GetValue() == allergensServedLimit1)
+            alienMovementSpeedUpgradesAvailable++;
+        if (allergensReference.GetValue() == allergensServedLimit2)
+            alienMovementSpeedUpgradesAvailable++;
+        if (allergensReference.GetValue() == allergensServedLimit3)
+            alienMovementSpeedUpgradesAvailable++;
+        CheckButton();
     }
-
     private void CheckButton()
     {
-        if (upgradesAvailable > 0)
-        {
+        if (movementSpeedUpgradesAvailable > 0)
             upgradeButton.interactable = true;
-        }
-    }
 
+        if (alienMovementSpeedUpgradesAvailable > 0)
+        {
+            //Enable UpgradeButtonHere for 
+        }
+        if (extraCookingStationUpgradesAvailable > 0)
+        {
+            //Enable ExtraCookingStationUpgrades for 
+        }
+        if (extraStorageSlot > 0)
+        {
+            //Enable UpgradeButtonHere for 
+        }
+        if (extraSeatingTable > 0)
+        {
+            //Enable UpgradeButtonHere for 
+        }
+
+    }
     public void UpgradeMS()
     {
-        Debug.Log("Upgraded something");
-        //Adjust movement depenign on how to adjust it i guess
-        upgradesAvailable--;
-
-        if (upgradesAvailable == 0)
+        if (tipsReference.GetValue() > movementSpeedUpgradeCost[currentMSUpgrade])
         {
-            upgradeButton.interactable = false;
+            tipsReference.ApplyChange(-movementSpeedUpgradeCost[currentMSUpgrade]);
+            Debug.Log("Upgraded something");
+            //Adjust movement depenign on how to adjust it i guess
+            movementSpeedUpgradesAvailable--;
+            currentMSUpgrade++;
+            if (movementSpeedUpgradesAvailable == 0)
+            {
+                upgradeButton.interactable = false;
+            }
         }
     }
-
     public void UpgradeCustomerMS()
     {
+        if (tipsReference.GetValue() > alienMovementSpeedUpgradeCost[currentCMSUpgrade])
+        {
+            tipsReference.ApplyChange(-alienMovementSpeedUpgradeCost[currentCMSUpgrade]);
+            //Adjust movement depenign on how to adjust it i guess
+            alienMovementSpeedUpgradesAvailable--;
+            currentCMSUpgrade++;
+            if (alienMovementSpeedUpgradesAvailable == 0)
+            {
+                upgradeButton.interactable = false;
+            }
+        }
+    }
+    public void UpgradeCookingStation()
+    {
+        if (tipsReference.GetValue() > cookingStationUpgradeSlot[currentCookingStationUpgrade])
+        {
+            tipsReference.ApplyChange(-cookingStationUpgradeSlot[currentCookingStationUpgrade]);
+            //Adjust movement depenign on how to adjust it i guess
+            
+            cookingStations[cookingStation].SetActive(false);
+            cookingStation++;
+            cookingStations[cookingStation].SetActive(true);
+            
+            alienMovementSpeedUpgradesAvailable--;
+            currentCMSUpgrade++;
+            if (alienMovementSpeedUpgradesAvailable == 0)
+            {
+                upgradeButton.interactable = false;
+            }
+        }
+    }
+    public void UpgradeTable()
+    {
+        tables[table].SetActive(false);
+        table++;
+        tables[table].SetActive(true);
+        
+        if (tipsReference.GetValue() > extraSeatingUpgradeCost[currentSeatingUpgrade])
+        {
+            tipsReference.ApplyChange(-extraSeatingUpgradeCost[currentSeatingUpgrade]);
+            //Adjust movement depenign on how to adjust it i guess
+            
+            //SPAWN TABLES IN DESIGNATED WAY
+            
+            alienMovementSpeedUpgradesAvailable--;
+            currentCMSUpgrade++;
+            if (alienMovementSpeedUpgradesAvailable == 0)
+            {
+                upgradeButton.interactable = false;
+            }
+        }
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            tipsReference.ApplyChange(+400);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            movementSpeedUpgradesAvailable++;
+        }
+        Debug.Log(tipsReference.GetValue());
     }
 }
